8a5ba2b8dde51869d70ee6c70a418c1706dda089
Dmitry <git@e965.ru>
2021-09-21 11:32:25 +0500

    fix

--- js/vk-token-check.js
+++ js/vk-token-check.js
@@ -3,7 +3,7 @@
 let isVKtokenActive = false
 
 if (VK_STORAGE_TOKEN_ITEM_NAME in localStorage) {
-	if ((Date.now() / 1000) > Number(localStorage['vk-token-expires-date'])) {
+	if ((Date.now() / 1000) > Number(localStorage[VK_STORAGE_TOKEN_DATE_ITEM_NAME])) {
 		localStorage.removeItem(VK_STORAGE_TOKEN_ITEM_NAME)
 	} else {
 		isVKtokenActive = true
